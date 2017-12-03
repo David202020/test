74a1e8393dd88aa5627b55bb23cc9ec9e325a947
David202020 <31577641+David202020@users.noreply.github.com>
2017-12-02 16:45:56 -0800

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,1 +1,26 @@
+<?php
+namespace David202020;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as C;
+class Main extends PluginBase implements Listener{
+public function onLoad(){
+                    $this->getLogger()->info("Plugin Loading");
+          }
+          public function onEnable(){
+					$this->getServer()->getPluginManager()->registerEvents($this,$this);
+                    $this->getLogger()->info("Enabled Plugin");
+          }
+          public function onDisable(){
+                    $this->getLogger()->info("Plugin Disabled");
+          }
+}
+public function onJoin(PlayerJoinEvent $event){
+   $player = $event->getPlayer();
+   $name = $player->getName();
+   $this->getServer()->broadcastMessage(C::GREEN."Ducky Scammed the Server$! He's a dick!")
 
